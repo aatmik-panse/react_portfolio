9bf123576839fb09871517d1d1e1d67fa5ecca3a
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-05-28 00:23:12 +0530

    home page completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,40 +6,43 @@ export default function Home() {
   return (
     <div id="home">
       <aside className="left-container">
-        <img src={dp} alt="" className="hImg" />
+        <img src={dp} alt="Profile" className="hImg" />
         <div id="hLogos">
           <a href="https://github.com/aatmik-panse">
             <img
               src="https://img.icons8.com/ios-glyphs/500/000000/github.png"
-              alt=""
+              alt="GitHub"
               className="hIcon"
             />
           </a>
-
-          <img
-            src="https://img.icons8.com/ios-glyphs/500/000000/linkedin.png"
-            alt=""
-            className="hIcon"
-            onClick={() => {
-              window.open("https://www.linkedin.com/in/aatmikpanse");
-            }}
-          />
-          <img
-            src="https://img.icons8.com/ios-glyphs/500/000000/twitter.png"
-            alt=""
-            className="hIcon"
-          />
+          <a href="https://www.linkedin.com/in/aatmikpanse">
+            <img
+              src="https://img.icons8.com/ios-glyphs/500/000000/linkedin.png"
+              alt="LinkedIn"
+              className="hIcon"
+            />
+          </a>
+          <a href="https://twitter.com">
+            <img
+              src="https://img.icons8.com/ios-glyphs/500/000000/twitter.png"
+              alt="Twitter"
+              className="hIcon"
+            />
+          </a>
         </div>
       </aside>
       <section className="right-container">
-        <h1 className="hName">Aatmik Panse</h1>
-        <h2 className="hTitle">Full Stack Developer</h2>
-        <p className="hDesc">
-          Hi! I'm Aatmik Panse, a full stack developer. I'm currently pursuing
-          my B.Tech in Computer Science from SST. I love to code and build
-          stuff. I'm a quick learner and I'm always looking for new
-          opportunities to learn and grow.
-        </p>
+        <div className="rightPara">
+          <h1 className="hName">Aatmik Panse</h1>
+          <h2 className="hTitle">Full Stack Developer</h2>
+          <p className="hDesc">
+            Hi! I'm Aatmik Panse, a full stack developer. I'm currently pursuing
+            my B.Tech in Computer Science from SST. I love to code and build
+            stuff. I'm a quick learner and I'm always looking for new
+            opportunities to learn and grow.
+          </p>
+          <button className="hButton">Contact Me</button>
+        </div>
       </section>
     </div>
   );
