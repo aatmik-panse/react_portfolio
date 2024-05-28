78bb365cfd93abebe6e910cb26b026caa3719a69
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-05-29 01:28:23 +0530

    skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./Skills.css";
+import img from "/Users/aatmikpanse/dev/portfolio/react-portfolio/src/desk-illustration.png";
+
+export default function Skills() {
+  return (
+    <div>
+      <div className="skills-container">
+        <h2>Skills</h2>
+        <div className="scroll">
+          <div className="m-scroll">
+            <span className="skill-item">
+              <img src={img} alt="HTML" />
+              <p>HTML</p>
+            </span>
+
+            <span className="skill-item">
+              <img src={img} alt="Blender" />
+              <p>Blender</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Figma" />
+              <p>Figma</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="JavaScript" />
+              <p>JavaScript</p>
+            </span>
+
+            <span className="skill-item">
+              <img src={img} alt="HTML" />
+              <p>HTML</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Blender" />
+              <p>Blender</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Figma" />
+              <p>Figma</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="JavaScript" />
+              <p>JavaScript</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="HTML" />
+              <p>HTML</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Blender" />
+              <p>Blender</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Figma" />
+              <p>Figma</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="JavaScript" />
+              <p>JavaScript</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="HTML" />
+              <p>HTML</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Blender" />
+              <p>Blender</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="Figma" />
+              <p>Figma</p>
+            </span>
+            <span className="skill-item">
+              <img src={img} alt="JavaScript" />
+              <p>JavaScript</p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
