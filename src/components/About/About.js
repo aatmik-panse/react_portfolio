865af60eaea06cfcf8df33bd5aff0c319c3e7016
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-05-30 09:09:42 +0530

    navbar dissapear on scroll

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,12 +8,12 @@ export default function About() {
       <div className="container">
         <div className="who-i-am">
           <h1>Who I am</h1>
-          <p>My name's Aatmik. I'm a Full Stack developer based in India.</p>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo sunt
-            neque unde enim repellat accusamus nihil, aliquid ex maiores,
-            dolores error itaque beatae. Est unde laboriosam, voluptates cum
-            officiis soluta?
+            My name's Aatmik. I'm a Full Stack developer based in India. I'm
+            passionate about coding and I love to learn new things. Lorem ipsum
+            dolor sit amet consectetur, adipisicing elit. Quo sunt neque unde
+            enim repellat accusamus nihil, aliquid ex maiores, dolores error
+            itaque beatae. Est unde laboriosam, voluptates cum officiis soluta?
           </p>
         </div>
         <div className="image-container">
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 import logo from "/Users/aatmikpanse/dev/portfolio/react-portfolio/src/burger-menu-left-svgrepo-com.png";
 
 export default function Navbar() {
-  return (
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      setShow(currentScrollY <= 100);
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return show ? (
     <div className="nb">
       <h1>Aatmik Panse</h1>
       <img src={logo} alt="logo" id="nbImg" />
     </div>
-  );
+  ) : null;
 }
