ee796b8c48ed7ad486f09c3fe102d4ec45cbbfe1
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-01 11:37:35 +0530

    typing effect added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,61 +1,63 @@
 import React from "react";
 import dp from "/Users/aatmikpanse/dev/portfolio/react-portfolio/src/dp.jpeg";
 import "./Home.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Skills from "../Skills/Skills";
+import Typing from "../Typing/Typing";
 
 export default function Home() {
   return (
-    <Router>
-      <div id="home">
-        <aside className="left-container">
-          <img src={dp} alt="Profile" className="hImg" />
-          <div id="hLogos">
-            <a href="https://github.com/aatmik-panse">
-              <img
-                src="https://img.icons8.com/ios-glyphs/500/000000/github.png"
-                alt="GitHub"
-                className="hIcon"
-              />
-            </a>
-            <a href="https://www.linkedin.com/in/aatmikpanse">
-              <img
-                src="https://img.icons8.com/ios-glyphs/500/000000/linkedin.png"
-                alt="LinkedIn"
-                className="hIcon"
-              />
-            </a>
-            <a href="https://twitter.com">
-              <img
-                src="https://img.icons8.com/ios-glyphs/500/000000/twitter.png"
-                alt="Twitter"
-                className="hIcon"
-              />
-            </a>
-          </div>
-        </aside>
-        <section className="right-container">
-          <div className="rightPara">
-            <h1 className="hName">Aatmik Panse</h1>
-            <h2 className="hTitle">Full Stack Developer</h2>
-            <p className="hDesc">
-              Hi! I'm Aatmik Panse, a full stack developer. I'm currently
-              pursuing my B.Tech in Computer Science from SST. I love to code
-              and build stuff. I'm a quick learner and I'm always looking for
-              new opportunities to learn and grow.
-            </p>
+    <div id="home">
+      <aside className="left-container">
+        <img src={dp} alt="Profile" className="hImg" />
+        <div id="hLogos">
+          <a href="https://github.com/aatmik-panse">
+            <img
+              src="https://img.icons8.com/ios-glyphs/500/000000/github.png"
+              alt="GitHub"
+              className="hIcon"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/aatmikpanse">
+            <img
+              src="https://img.icons8.com/ios-glyphs/500/000000/linkedin.png"
+              alt="LinkedIn"
+              className="hIcon"
+            />
+          </a>
+          <a href="https://twitter.com">
+            <img
+              src="https://img.icons8.com/ios-glyphs/500/000000/twitter.png"
+              alt="Twitter"
+              className="hIcon"
+            />
+          </a>
+        </div>
+      </aside>
+      <section className="right-container">
+        <div className="rightPara">
+          <h1 className="hName">Aatmik Panse</h1>
+          <Typing
+            text={[
+              "Full Stack Developer",
+              "Web Developer",
+              "React Developer",
+              "Swift Developer",
+              "Android Developer",
+            ]}
+            speed={120}
+            eraseSpeed={80}
+            eraseDelay={3000}
+            loop={true}
+          />
+          <p className="hDesc">
+            Hi! I'm Aatmik Panse. I'm currently pursuing my B.Tech in Computer
+            Science from SST. I love to code and build stuff. I'm a quick
+            learner and I'm always looking for new opportunities to learn and
+            grow.
+          </p>
 
-            <button className="hButton">
-              Contact Me
-              <Link to="/about"></Link>
-            </button>
-          </div>
-        </section>
-        <Routes>
-          {/* <Route path="/about" element={<Skills />} />{" "} */}
-          {/* <Route path="/" element={<Home />} /> */}
-        </Routes>
-      </div>
-    </Router>
+          <button className="hButton">Contact Me</button>
+        </div>
+      </section>
+    </div>
   );
 }
---
+++ src/components/Typing/Typing.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./Typing.css";
+
+function Typing({
+  text,
+  typingSpeed = 100,
+  deletingSpeed = 50,
+  duration = 1000,
+}) {
+  const [displayedText, setDisplayedText] = useState("");
+  const [isDeleting, setisdeleting] = useState(false);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const handleTyping = () => {
+      if (!isDeleting) {
+        if (displayedText.length < text[index].length) {
+          setDisplayedText(
+            (prev) => prev + text[index].charAt(displayedText.length)
+          );
+        } else {
+          setTimeout(() => setisdeleting(true), duration);
+        }
+      } else {
+        if (displayedText.length > 0) {
+          setDisplayedText((prev) => prev.slice(0, -1));
+        } else {
+          setisdeleting(false);
+          setIndex((prev) => (prev + 1) % text.length);
+        }
+      }
+    };
+
+    const timeout = setTimeout(
+      handleTyping,
+      isDeleting ? deletingSpeed : typingSpeed
+    );
+
+    return () => clearTimeout(timeout);
+  }, [
+    displayedText,
+    isDeleting,
+    index,
+    text,
+    typingSpeed,
+    deletingSpeed,
+    duration,
+  ]);
+  return (
+    <div className="typing-effect">
+      {displayedText}
+      <span className="carret"></span>
+    </div>
+  );
+}
+export default Typing;
