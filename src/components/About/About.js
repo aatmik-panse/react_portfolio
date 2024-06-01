1158367cdb66fd0538c432236f93f970cd356185
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-02 00:45:13 +0530

    updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,11 +8,11 @@ export default function About() {
         <div className="who-i-am">
           <h1>Who I am</h1>
           <p>
-            My name's Aatmik. I'm a Full Stack developer based in India. I'm
-            passionate about coding and I love to learn new things. Lorem ipsum
-            dolor sit amet consectetur, adipisicing elit. Quo sunt neque unde
-            enim repellat accusamus nihil, aliquid ex maiores, dolores error
-            itaque beatae. Est unde laboriosam, voluptates cum officiis soluta?
+            Hi! I'm Aatmik Panse, a Computer Science student with a keen
+            interest in software development and technology. Studying at Scaler
+            School of Technology, I'm passionate about learning and applying
+            coding concepts to solve real-world problems. I'm eager to
+            contribute my skills and knowledge to create innovative solutions.
           </p>
         </div>
         <div className="image-container">
